a9e453437303f8dbe7a44ce403eb3aa320965533
Aashutosh Basnet <aashutoshbasnet3344@gmail.com>
2024-12-05 20:59:58 +0530

    "hero section beta stage"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero />
+      </main>
+    </div>
+  );
+};
+
+export default App;
